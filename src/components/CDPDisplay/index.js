134ecc1fb684f3501c296aef94682a00fa225d67
Restyled.io <commits@restyled.io>
2019-11-25 10:38:05 +0000

    Restyled by prettier

--- src/components/CDPDisplay/index.js
+++ src/components/CDPDisplay/index.js
@@ -2,23 +2,23 @@ import useMaker from 'hooks/useMaker';
 import useSidebar from 'hooks/useSidebar';
 import useStore from 'hooks/useStore';
 import LoadingLayout from 'layouts/LoadingLayout';
-import React, {useEffect, useMemo, useState} from 'react';
-import {hot} from 'react-hot-loader/root';
-import {useNavigation} from 'react-navi';
-import {getCdp} from 'reducers/cdps';
-import {trackCdpById} from 'reducers/multicall/cdps';
-import {getColor} from 'styles/theme';
+import React, { useEffect, useMemo, useState } from 'react';
+import { hot } from 'react-hot-loader/root';
+import { useNavigation } from 'react-navi';
+import { getCdp } from 'reducers/cdps';
+import { trackCdpById } from 'reducers/multicall/cdps';
+import { getColor } from 'styles/theme';
 
-import {Routes, ZERO_ADDRESS} from '../../utils/constants';
+import { Routes, ZERO_ADDRESS } from '../../utils/constants';
 import Unavailable from '../Unavailable';
 
 import CDPViewPresentation from './Presentation';
 
-function CDPView({cdpId}) {
+function CDPView({ cdpId }) {
   cdpId = parseInt(cdpId, 10);
-  const {maker, account, network} = useMaker();
-  const {show : showSidebar} = useSidebar();
-  const [{cdps, feeds}, dispatch] = useStore();
+  const { maker, account, network } = useMaker();
+  const { show: showSidebar } = useSidebar();
+  const [{ cdps, feeds }, dispatch] = useStore();
   const [cdpOwner, setOwner] = useState();
   const [cdpAvailable, setCdpAvailable] = useState(true);
   const navigation = useNavigation();
@@ -26,18 +26,22 @@ function CDPView({cdpId}) {
   useEffect(() => {
     (async () => {
       async function redirect(account) {
-        const {search} = (await navigation.getRoute()).url;
-        navigation.navigate(
-            {pathname : `/${Routes.BORROW}/owner/${account.address}`, search});
+        const { search } = (await navigation.getRoute()).url;
+        navigation.navigate({
+          pathname: `/${Routes.BORROW}/owner/${account.address}`,
+          search
+        });
       }
-      const proxyAddress =
-          await maker.service('mcd:cdpManager').getOwner(cdpId);
+      const proxyAddress = await maker
+        .service('mcd:cdpManager')
+        .getOwner(cdpId);
       if (proxyAddress === ZERO_ADDRESS) {
         account ? redirect(account) : setCdpAvailable(false);
       } else {
         try {
-          const cdpOwnerAddress =
-              await maker.service('proxy').getOwner(proxyAddress);
+          const cdpOwnerAddress = await maker
+            .service('proxy')
+            .getOwner(proxyAddress);
           setOwner(cdpOwnerAddress);
         } catch (err) {
           // cdp is not owned by a proxy
@@ -45,7 +49,7 @@ function CDPView({cdpId}) {
         }
       }
     })();
-  }, [ maker, cdpId, account, navigation ]);
+  }, [maker, cdpId, account, navigation]);
 
   // this workaround (making useMemo depend on just one feed item) ensures that
   // the view does not re-render when an irrelevant price feed is updated.
@@ -54,11 +58,15 @@ function CDPView({cdpId}) {
   // about how it shows up in different situations.
   const ilk = cdps[cdpId] ? cdps[cdpId].ilk : null;
   const feed = ilk ? feeds.find(f => f.key === ilk) : null;
-  const cdp = useMemo(() => getCdp(cdpId, {cdps, feeds : [ feed ]}),
-                      [ cdpId, cdps, feed ]);
+  const cdp = useMemo(() => getCdp(cdpId, { cdps, feeds: [feed] }), [
+    cdpId,
+    cdps,
+    feed
+  ]);
 
-  useEffect(() => { trackCdpById(maker, cdpId, dispatch); },
-            [ cdpId, dispatch, maker ]);
+  useEffect(() => {
+    trackCdpById(maker, cdpId, dispatch);
+  }, [cdpId, dispatch, maker]);
 
   return useMemo(
     () =>
@@ -68,14 +76,12 @@ function CDPView({cdpId}) {
           showSidebar={showSidebar}
           account={account}
           network={network}
-          cdpOwner={
-    cdpOwner.toLowerCase()}
+          cdpOwner={cdpOwner.toLowerCase()}
         />
       ) : !cdpAvailable ? (
         <Unavailable />
       ) : (
-        <LoadingLayout background={
-    getColor('lightGrey')} />
+        <LoadingLayout background={getColor('lightGrey')} />
       ),
     [cdp, showSidebar, account, network, cdpOwner, cdpAvailable]
   );
